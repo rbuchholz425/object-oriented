4fd03420420dcbb82d1fc8bd66d7d15641362ce8
Ryan Buchholz <rbuchholz425@gmail.com>
2020-01-23 08:31:27 -0700

    Declared State Variables.

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -1,1 +1,44 @@
 <?php
+ /**
+  *  Author of something.
+  *
+  * This Author class will hold the data that it has typed into it.
+  *
+  * @author Ryan Buchholz <rbuchholz1>
+  */
+
+ class Author {
+ 	 /**
+ 	  * @var $authorId
+ 	  */
+ 	private $authorId;
+ 	/**
+ 	 * This is the author's Id.
+	 * @var$authorActivationToken
+ 	 */
+ 	private $authorActivationToken;
+ 	/**
+ 	 * This is the author's activation token for their account.
+	 * @var $authorAvatarUrl
+ 	 */
+ 	 private $authorAvatarUrl;
+ 	 /**
+ 	  * This is the author's Avatar URL for their image.
+	  * @var $authorEmail
+ 	  */
+ 	 private $authorEmail;
+ 	 /**
+ 	  * This is the author's Email linked to their account.
+	  * @var $authorHash
+ 	  */
+ 	 private $authorHash;
+ 	 /**
+ 	  * This is the author's password.
+	  * @var $authorUsername
+ 	  */
+ 	 private $authorUsername;
+ 	 /**
+ 	  * This is the author's user name linked to their account.
+ 	  */
+
+ }
