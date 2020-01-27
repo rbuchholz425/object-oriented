0ea80014727be2816222df69370565e3cc5e3022
Ryan Buchholz <rbuchholz425@gmail.com>
2020-01-27 16:42:05 -0700

    complete instantiation, boss fight completed.

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -10,4 +10,4 @@ $hash = password_hash("password", PASSWORD_ARGON2I, ["time_cost" => 7]);
 
 $newAuthor = new Author("0850d78d-0ff3-402d-a783-273385ef778d", "12345678901234567890123456789012", "myprofilepic.com", "author1234@gmail.com", $hash, "JRRTolkien");
 
-echo $newAuthor->getAuthorUsername();
+echo ($newAuthor-> getAuthorId()),($newAuthor-> getAuthorActivationToken()),($newAuthor-> getAuthorAvatarUrl()),($newAuthor-> getAuthorEmail()),($newAuthor-> getAuthorHash()),($newAuthor-> getAuthorUsername());
